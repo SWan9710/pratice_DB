e460604b105a5d685f115a369016ac52aa8c6324
krkdhs6240 <wjdtndhks1212@gmail.com>
2023-04-14 17:51:03 +0900

    230414 css pratice

---
+++ 230412/query_pjt/myapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Pet, Petsitter
+
+# Register your models here.
+admin.site.register(Pet)
+admin.site.register(Petsitter)
---
+++ 230412/query_pjt/myapp/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.db import connection
+from django.db import reset_queries
+from .models import Petsitter, Pet
+
+# Create your views here.
+
+def get_sql_queries(origin_func):
+    def wrapper():
+        reset_queries()
+        origin_func()
+        query_info = connection.queries
+        for query in query_info:
+            print(query['sql'])
+    return wrapper
+
+@get_sql_queries
+def get_pet_data():
+    reset_queries()
+    pets = Pet.objects.all()
+    for pet in pets:
+        print(f'{pet.name} | 집사 {pet.pet_sitter.first_name}')
+
+    
